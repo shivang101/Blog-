5fbfaba0ae1817c98f644b5de807542cfeff6263
shivang <shivangmathur101@gmail.com>
2022-01-11 16:55:58 +0530

    feature of deleteing blogs

--- src/BLogDetails.js
+++ src/BLogDetails.js
@@ -1,14 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const {
     data: blog,
     error,
     isPendind,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -19,6 +29,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
